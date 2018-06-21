d58723efcb73babfd590e40258aad724723075ac
Michael Dunlap <michaeldunlap@earthlink.net>
2018-06-21 12:17:59 -0700

    create basic input and render it in App

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'w3-css/w3.css'
 
+import SearchBar from './components/SearchBar'
+
+
 const API_KEY = 'AIzaSyBJkjEBpcKIJesdlY-Mt_1WQEmoJUxLzog'
 
 const App = () =>
-  <div>Fledgling YouTube Search</div>
+  <SearchBar />
 
 ReactDOM.render(<App />, document.getElementById('app'))
