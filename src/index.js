7bf9d5a1e93928432a7827392f911b464a6ca5af
Michael Dunlap <michaeldunlap@earthlink.net>
2018-06-21 12:13:31 -0700

    add API key for youtube and install youtube-api-search package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'w3-css/w3.css'
 
+const API_KEY = 'AIzaSyBJkjEBpcKIJesdlY-Mt_1WQEmoJUxLzog'
+
 const App = () =>
   <div>Fledgling YouTube Search</div>
 
