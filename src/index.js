806a51029e8437da2bf15d8624633d3c72d630aa
Michael Dunlap <michaeldunlap@earthlink.net>
2018-06-21 14:06:19 -0700

    grab videos using youtube api and print in console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import 'w3-css/w3.css'
 
 import SearchBar from './components/SearchBar'
 
 
 const API_KEY = 'AIzaSyBJkjEBpcKIJesdlY-Mt_1WQEmoJUxLzog'
+YTSearch({ key: API_KEY, term: 'Mahler' }, data => console.log(data))
 
 const App = () =>
   <SearchBar />
