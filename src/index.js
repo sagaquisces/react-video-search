a409e5f4021956fa442c947f6e19c29575266124
Michael Dunlap <michaeldunlap@earthlink.net>
2018-06-21 14:14:24 -0700

    add state to App to take in videos from youtube via api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import 'w3-css/w3.css'
@@ -7,9 +7,23 @@ import SearchBar from './components/SearchBar'
 
 
 const API_KEY = 'AIzaSyBJkjEBpcKIJesdlY-Mt_1WQEmoJUxLzog'
-YTSearch({ key: API_KEY, term: 'Mahler' }, data => console.log(data))
 
-const App = () =>
-  <SearchBar />
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({ key: API_KEY, term: 'Mahler' }, videos => this.setState({ videos }))
+  }
+  render() {
+    return (
+      <SearchBar />
+    )
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
