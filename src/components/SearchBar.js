eceee51a0906c997ba01eb867cf0bd90e5231020
Michael Dunlap <michaeldunlap@earthlink.net>
2018-06-21 13:48:44 -0700

    SearchBar is now a controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,8 +10,10 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <input onChange={e => this.setState({ term: e.target.value })}/>
-        Value of input: {this.state.term}
+        <input
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })}
+        />
       </div>
     )
   }
